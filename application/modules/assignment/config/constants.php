446df7e0be025bc01e2cb98cfd35241c8252a12f
vivekanand25jan <40717383+vivekanand25jan@users.noreply.github.com>
2025-05-14 01:40:24 +0400

    Update constants.php

--- application/modules/assignment/config/constants.php
+++ application/modules/assignment/config/constants.php
@@ -17,16 +17,16 @@ define('FILE_READ_MODE', 0644);
 define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
 define('DIR_WRITE_MODE', 0777);
-define('FOOTER_NAME', 'Travel Point LLC');
+define('FOOTER_NAME', 'BITS');
 define('VERSION', '2.4.0');
-define('MFG_YEAR', '2020-2021');
-define('COMP_URL', 'http://travelpoint.om');
-define('ENDPOINT', 'https://api.test.hotelbeds.com');
-define('SECURE_ENDPOINT', 'https://api-secure.test.hotelbeds.com');
-define('API_KEY', 'da8ba0d1303ea39e6ae164c0af54c2f7');
-define('SECRET', '695df0f9d1');
+define('MFG_YEAR', '2025');
+define('COMP_URL', 'http://bits.com.in');
+define('ENDPOINT', '#');
+define('SECURE_ENDPOINT', '#');
+define('API_KEY', '#');
+define('SECRET', '#');
 define('FORMATE', 'application/json');
-define('TABLE_RECORD','staticTabHotelbedsTotalDataCount');
+define('TABLE_RECORD','#');
 
 /*
 |--------------------------------------------------------------------------
